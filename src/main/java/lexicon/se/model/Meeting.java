82a5f072155c09ca05940c4b5fcac24a0fcd3f3b
Bigom <bigom2199@gmail.com>
2023-10-02 18:44:49 +0200

    implement meeting and MeetingCalendar classes

--- src/main/java/lexicon/se/model/Meeting.java
+++ src/main/java/lexicon/se/model/Meeting.java
@@ -1,5 +1,78 @@
 package lexicon.se.model;
 
+import java.time.LocalDateTime;
+
 public class Meeting {
+     private int id;
+     private String title;
+     private LocalDateTime startTime;
+     private LocalDateTime endTime;
+     private String description;
+     private MeetingCalendar calendar;
+
+    public Meeting(int id, String title, LocalDateTime startTime, LocalDateTime endTime, String description, MeetingCalendar calendar) {
+        this(id,title,startTime,endTime,description);
+
+        this.calendar = calendar;
+    }
+
+    public Meeting(String title, LocalDateTime startTime, LocalDateTime endTime, String description) {
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
+
+    public Meeting(String title, LocalDateTime startTime, LocalDateTime endTime, String description, MeetingCalendar calendar) {
+        this(title,startTime,endTime,description);
+
+        this.calendar = calendar;
+    }
+
+    public Meeting(int id, String title, LocalDateTime startTime, LocalDateTime endTime, String description) {
+        this.id = id;
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public MeetingCalendar getCalendar() {
+        return calendar;
+    }
+    public String meetingInfo(){
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder.append("Meeting Info:").append("\n");
+        StringBuilder.append("Id").append("\n");
+        StringBuilder.append("Title").append(title).append("\n");
+        StringBuilder.append("StartTime").append(startTime).append("\n");
+        StringBuilder.append("endTime").append(endTime).append("\n");
+        StringBuilder.append("Description").append(description).append("\n");
+        return StringBuilder.toString();
+    }
+   private void timeValidation(){
+        // todo: implement later
+   }
 
 }
+
--- src/main/java/lexicon/se/model/MeetingCalendar.java
+++ src/main/java/lexicon/se/model/MeetingCalendar.java
@@ -1,5 +1,69 @@
 package lexicon.se.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MeetingCalendar {
+       private int id;
+       private String username;
+       private String title ;
+    private List<Meeting> meetings;
+    public MeetingCalendar(String username,String title){
+        this(title);
+        this.username = username;
+
+    }
+    public MeetingCalendar(String title ){
+        this.title= title;
+    }
+
+    public MeetingCalendar(int id, String username, String title) {
+        this(username, title);
+        this.id = id;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Meeting> getMeetings() {
+        if (meetings == null) meetings = new ArrayList<Meeting>();
+        return meetings;
+    }
+
+    public void addMeeting(Meeting meeting){
+   if (meetings == null) meetings = new ArrayList<Meeting>();
+   meetings.add(meeting);
+    }
+     public void removeMeeting(Meeting meeting){
+        if (meetings == null) throw new IllegalArgumentException("Meeting list is null.");
+        if (meeting == null) throw  new IllegalArgumentException("Meeting date is null.");
+        meetings.remove(meeting);
+
+     }
+    public String calendarInfo(){
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder.append("Calendar Info:").append("\n");
+        StringBuilder.append("Id").append(id).append("\n") ;
+        StringBuilder.append("Title").append(title).append("\n");
+        StringBuilder.append("Username").append(username).append("\n");
+        return StringBuilder.toString();
+
+
+    }
+
+
+
+
 
 }
