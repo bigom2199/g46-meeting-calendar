62de6bc6071ff4486648fe8511928fe05eacb52f
Bigom <bigom2199@gmail.com>
2023-10-02 16:16:04 +0200

    define user model

--- src/main/java/lexicon/se/model/User.java
+++ src/main/java/lexicon/se/model/User.java
@@ -1,10 +1,19 @@
 package lexicon.se.model;
 
-public class User {
-    private String username;
-    private String password;
-    // todo: define constructors
+import java.security.SecureRandom;
+import java.util.Random;
 
+public class User {
+    private final String username;
+    private String password;
+    public User(String username){
+        this.username = username;
+        newPassword();
+    }
+  public User(String username, String password){
+        this.username = username;
+        this.password = password;
+  }
 
     public String getUsername(){
         return username;
@@ -17,6 +26,22 @@ public class User {
 
     }
     public void newPassword(){
-        // todo: assign a new password to password field
+        this.password = generateRandomPassword();
+
+
+    }
+    private String generateRandomPassword(){
+        String allowedCharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        int passwordLength= 10;
+        StringBuilder stringBuilder = new StringBuilder();
+         Random random =new SecureRandom();
+         for (int i= 0; i< passwordLength;i++){
+             int randomIndex = random.nextInt(allowedCharacters.length());
+             char randomChar = allowedCharacters.charAt(randomIndex);
+
+
+         }
+
+        return StringBuilder.toString() ;
     }
 }
